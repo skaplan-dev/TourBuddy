bc830f619639c419024b9ab33a2e733ca247578b
Seth Kaplan <sethkaplan73@gmail.com>
2019-07-26 09:18:42 -0400

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,15 +2,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyD9evS0KtRRLXwX--rrOZOrm_NP8g3Hkjo",
-    authDomain: "tour-buddy-e0aa0.firebaseapp.com",
-    databaseURL: "https://tour-buddy-e0aa0.firebaseio.com",
-    projectId: "tour-buddy-e0aa0",
-    storageBucket: "tour-buddy-e0aa0.appspot.com",
-    messagingSenderId: "21551209412",
-    appId: "1:21551209412:web:1b527455494dce33"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   },
   mapbox: {
-    accessToken: "pk.eyJ1Ijoic21pbGVyayIsImEiOiJjanhqZTBodGkyNXMwM3Vwa2FzaXB2OWphIn0.63cSQS_T7Lqcd1pALRhuSA"
+    accessToken: ""
   }
 };
